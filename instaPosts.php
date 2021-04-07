a9253fc6a6d9bb1c9f5db4a16b8087d0107c7c7e
EiadElrefaiy <71601178+EiadElrefaiy@users.noreply.github.com>
2021-04-08 01:05:09 +0200

    Update instaPosts.php

--- instaPosts.php
+++ instaPosts.php
@@ -81,7 +81,7 @@
    <div><a href="https://www.facebook.com/TanTaClub/posts/10159087210637165"><input class="post1" value="Post 6" type="button"></a></div>
    <div><a href="https://www.facebook.com/TantaToday/posts/1420061568346869"><input class="post1" value="Post 7" type="button"></a></div>
    <div><a href="https://www.facebook.com/Tantaalaan/posts/2014188542055065"><input class="post1" value="Post 8" type="button"></a></div>
-   <div><a href="https://www.facebook.com/ElMansourasc/posts/1621099261416306" value="Post 9" type="button"></a></div>
+   <div><a href="https://www.facebook.com/ElMansourasc/posts/1621099261416306"><input class="post1" value="Post 9" type="button"></a></div>
    <div><a href="https://www.facebook.com/MoHaMeD.7PiP/posts/2919917711466517"><input class="post1" value="Post 10" type="button"></a></div>
   
  
